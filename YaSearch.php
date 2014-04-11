501eeb219cf34979835e54f4546d91b6372fe58e
Nikita Kotenko <nick.w2r@gmail.com>
2014-04-11 18:34:38 +0300

    start

---
+++ YaSearch.php
@@ -0,0 +1,17 @@
+<?php
+namespace samson\yasearch;
+
+use samson\core\CompressableExternalModule;
+
+
+/**
+ * Class for working with yandex site search engine
+ *
+ * @package SamsonPHP
+ * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
+ * @version 1.0
+ */
+class YaSearch extends CompressableExternalModule
+{
+	
+}
---
+++ ya_search.js
@@ -0,0 +1,44 @@
+var yasearch = function(form){
+
+    // Поле ввода на сайте
+    var sf = s('input.yafield', form);
+
+    // Форма яндекса долго загружается, повесим событие на фокус
+    sf.focus(function()
+    {
+        // Поле ввода для поиска
+        var yaf = s('.ya-site-form__input input');
+
+        // Кнопка начала поиска
+        var yab = s('.ya-site-form__submit');
+
+        // Обработчик отправки формы сайта
+        form.submit( function()
+        {
+            s.trace(sf.val());
+
+            yaf.val( sf.val() );
+
+            yab.click();
+
+            return false;
+
+        }, true, true);
+
+        // Обработчик ввода данных в поле ввода
+        if( yaf ) sf.keyup(function( o, opt,e )
+        {
+            // Нажали enter - все убираем
+            if (e.keyCode == 13){
+                yaf.val( sf.val() );
+                yab.click();
+            }
+
+            // Нажали esc - все убираем
+            if (e.keyCode == 27) { sf.val(''); sf.blur(); }
+        });
+    });
+};
+
+// Bind yasearhc init on dom load
+s('form.yasearch').pageInit(yasearch);
